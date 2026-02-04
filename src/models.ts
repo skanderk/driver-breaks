c7b488a632a6f30aa7d20beb70349e6d46ebdffd
Skander Kort <skanderkort@gmail.com>
2026-02-04 11:39:10 -0500

    Implemented models for slots, schedules, infringements.
    Implemented interfaces and types for time slots,
    schedules and infringements.

---
+++ src/models.ts
@@ -0,0 +1,46 @@
+/**
+ * Describes a timeslot in a driver's schedule.
+ * - drive: The driver is driving during this time slot.
+ * - wait: The driver is waiting at a customer site for the service time window to open.
+ * - serviceCustomer: The driver is servicing a customer (loading/unloading goods) or waiting 
+ * for the vehicle to loaded/unloaded.
+ * - otherWork: Other type of work: paper work, fuelling vehicle...
+ * - break: The driver is taking a mandatory break per article 7.
+ * - rest: Daily rest period.   
+ */
+export type TimeSlotType = 'drive' | 'wait' | 'serviceCustomer' | 'otherWork' | 'break' | 'rest'; 
+
+/**
+ * An immutable slot in a daily schedule of a driver.
+ */
+export interface TimeSlot {
+    readonly slotType: TimeSlotType 
+    readonly durationSeconds: number
+}
+
+export interface DailySchedule {
+    day: string,
+    slotsSequence: TimeSlot[]
+}
+
+/**
+ * The schedule of a driver over a horizon. 
+ * @note We assume that a horizon covers a period shorter than one week as this first
+ * iteration does not cover weekly rest periods...
+ */
+export  type HorizonSchedule = DailySchedule[];
+
+/**
+ * An infringement of regulations 561/2006.
+ * - articleNumber: The number of the infringed article.
+ * - brokenRule: The rule broken within this article (free form text).
+ * - offendingDay: The day when this infringement was detected.
+ * - offendingSlotIndex: The index on the offending time slot in the offending daily 
+ * schedule. 
+ */
+export interface Infringement {
+    articleNumber: number
+    brokenRule: string
+    day: string
+    offendingSlotIndex: number  
+}
