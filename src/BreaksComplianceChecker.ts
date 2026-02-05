d9c6509111bcf68b66f3f9c1401928ffb7c97943
Skander Kort <skanderkort@gmail.com>
2026-02-04 19:22:03 -0500

    Grouped compliance checker state variables.
    Grouped BraksComplianceChecker "state" variables in a state
    property of type CheckerState.

--- src/BreaksComplianceChecker.ts
+++ src/BreaksComplianceChecker.ts
@@ -2,6 +2,8 @@ import { ComplianceChecker } from './ComplianceChecker'
 import { ArticleNumber, DailySchedule, HorizonSchedule, Infringement } from './models'
 
 export class BreaksComplianceChecker implements ComplianceChecker {
+
+    private state: CheckerState;
     /**
      * We inject the durations in the constructor to make this compliance 
      * checker a bit more generic. Also this DI allows us to enforce some properties/assumptions 
@@ -29,6 +31,8 @@ export class BreaksComplianceChecker implements ComplianceChecker {
         if (splitBreakDurations[0] > splitBreakDurations[1]) {
             throw new Error('First part of a split break must be shorter than the second part');
         }
+
+        this.state = new CheckerState();
     }
 
     private isValidDuration(durationSec: number): void {
@@ -45,7 +49,7 @@ export class BreaksComplianceChecker implements ComplianceChecker {
 
     /**
      * 
-     * @returns @inheritdoc
+     * @inheritdoc
      */
     public getArticle(): ArticleNumber {
         return new ArticleNumber(7);
@@ -66,11 +70,8 @@ export class BreaksComplianceChecker implements ComplianceChecker {
     }
 
     private checkDailyCompliance(schedule: DailySchedule): Infringement[] {
-        let infringements: Array<Infringement> = [];
-
-        let drivingTime = 0; // Driving time since last break.
-        let takingSplitBreak = false; // true if the driver is taking a split break.
-        let infringementReported = false; // Prevents reporting the same infringement multiple times.
+        // Reset state before each compliance check.
+        this.state = new CheckerState();
 
         for (let i = 0; i < schedule.slotsSequence.length; i++) {
             const currentSlot = schedule.slotsSequence[i];
@@ -78,47 +79,47 @@ export class BreaksComplianceChecker implements ComplianceChecker {
 
             switch (currentSlot.slotType) {
                 case 'drive':
-                    drivingTime += curSlotDurationSec;
+                    this.state.drivingTime += curSlotDurationSec;
 
                     // We allow the driver to exceed maxDrivingDurationSec if they are taking a rest right afterwards.
-                    if (drivingTime > this.maxDrivingDurationSec && infringementReported === false &&
+                    if (this.state.drivingTime > this.maxDrivingDurationSec && this.state.infringementReported === false &&
                         (!this.isLastSlot(schedule, i) && schedule.slotsSequence[i + 1].slotType !== 'rest')) {
-                        infringements.push({
+                        this.state.infringements.push({
                             articleNumber: this.getArticle(),
                             day: schedule.day,
-                            brokenRule: `Driver did not take a break: drive time without a break ${drivingTime}`,
+                            brokenRule: `Driver did not take a break: drive time without a break ${this.state.drivingTime}`,
                             offendingSlotIndex: i
                         });
-                        infringementReported = true;
+                        this.state.infringementReported = true;
                     }
                     break;
                 case 'rest':
-                    drivingTime = 0; // Just in case the rest happens in the middle of a daily schedule.
-                    infringementReported = false;
-                    takingSplitBreak = false;
+                    this.state.drivingTime = 0; // Just in case the rest happens in the middle of a daily schedule.
+                    this.state.infringementReported = false;
+                    this.state.takingSplitBreak = false;
                     break;
                 case 'break':
                     if (curSlotDurationSec >= this.singleBreakDurationSec) {
-                        drivingTime = 0;
-                        infringementReported = false;
-                        takingSplitBreak = false;
+                        this.state.drivingTime = 0;
+                        this.state.infringementReported = false;
+                        this.state.takingSplitBreak = false;
                     } else { // This is a split break
-                        if (!takingSplitBreak) {
+                        if (!this.state.takingSplitBreak) {
                             if (curSlotDurationSec >= this.splitBreakDurations[0]) {
-                                takingSplitBreak = true;
+                                this.state.takingSplitBreak = true;
                             } else {
-                                infringements.push({
+                                this.state.infringements.push({
                                     articleNumber: this.getArticle(),
                                     day: schedule.day,
                                     brokenRule: `Driver took an insufficient split break: 1st break duration ${curSlotDurationSec}`,
                                     offendingSlotIndex: i
                                 });
                             }
-                        } else { // The driver already took the first part of a split break.
+                        } else { // The driver already took the first part of a split break and this is the second one.
                             if (curSlotDurationSec >= this.splitBreakDurations[1]) {
-                                drivingTime = 0;
+                                this.state.drivingTime = 0;
                             } else {
-                                infringements.push({
+                                this.state.infringements.push({
                                     articleNumber: this.getArticle(),
                                     day: schedule.day,
                                     brokenRule: `Driver took an insufficient split break: 2nd break duration ${curSlotDurationSec}`,
@@ -128,20 +129,34 @@ export class BreaksComplianceChecker implements ComplianceChecker {
                                 // We still reset drivingTime to allow the checker to recover from this infrigement and detect 
                                 // other enfrigements down the road if any (think compilers trying to recover from an error 
                                 // in order to detect other errors in the program).
-                                drivingTime = 0;
+                                this.state.drivingTime = 0;
                             }
 
-                            takingSplitBreak = false; // Reset flag in either case.
-                            infringementReported = false;
+                            this.state.takingSplitBreak = false; // Reset flag in either case.
+                            this.state.infringementReported = false;
                         }
                     }
             }
         }
 
-        return infringements;
+        return this.state.infringements;
     }
 
     private isLastSlot(schedule: DailySchedule, slotIndex: number): boolean {
         return slotIndex === schedule.slotsSequence.length - 1;
     }
+}
+
+
+/**
+ * Mutable per-day state used by `BreaksComplianceChecker` while scanning a schedule.
+ * This tracks accumulated driving time, split-break progress, and collected infringements
+ * so the checker can recover after reporting and continue analysis.
+ */
+class CheckerState {
+    infringements: Array<Infringement> = [];
+
+    drivingTime = 0; // Driving time since last break.
+    takingSplitBreak = false; // true if the driver is taking a split break.
+    infringementReported = false; // Prevents reporting the same infringement multiple times.
 }
